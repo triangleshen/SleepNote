62f2d30a397afee8a64e93934fe1f6a7bde18244
triangleshen <wl00515922@gmail.com>
2022-04-24 20:20:21 +0800

    測驗RWD更新

---
+++ js/topic.js
@@ -0,0 +1,9 @@
+window.addEventListener( "load" , function (){
+    var screenWidth = document.body.clientWidth;
+    if(screenWidth <= 992){
+        $id('EPpic').src= 'pic/topic2_sm.jpg';
+        $id('SBpic').src= 'pic/topic3_sm.jpg';
+        $id('OTHERpic').src= 'pic/topic4_sm.jpg';
+        $id('Qpic').src= 'pic/topic_q_sm.jpg';
+    }
+});
